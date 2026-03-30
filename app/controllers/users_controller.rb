08eb25cd70421753d1921e39c59e648c51d94cab
blizzard115 <fubuki.sato@gmail.com>
2026-03-30 18:40:33 +0900

    マイページの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.order(created_at: :desc)
+    @posts = @user.posts.includes(:read_users).order(created_at: :desc)
 
     if params[:keyword].present?
       @posts = @posts.where("title LIKE ? OR content LIKE ?", "%#{params[:keyword]}%", "%#{params[:keyword]}%")
     end
 
-    if params[:status].present?
-      @posts = @posts.where(status: params[:status])
-    end
-
     if params[:post_type].present?
       @posts = @posts.where(post_type: params[:post_type])
+    end
+
+    if params[:read_status] == "unread" && user_signed_in?
+      read_post_ids = current_user.reads.select(:post_id)
+      @posts = @posts.where.not(id: read_post_ids).where.not(user_id: current_user.id)
     end
 
     @post_count = @posts.count
