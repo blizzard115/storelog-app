086fe51d0ae918af44664889e04385f932b40db2
blizzard115 <fubuki.sato@gmail.com>
2026-06-24 16:02:47 +0900

    Restrict read creation to current store posts

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -2,7 +2,7 @@ class ReadsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @post = Post.find(params[:post_id])
+    @post = current_user.store.posts.find(params[:post_id])
     current_user.reads.find_or_create_by(post: @post)
 
     redirect_to post_path(@post), notice: "確認しました"
